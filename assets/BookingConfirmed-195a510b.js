97bc65e3029c50d885da2fa7f33f885923509198
JacobJanashvili <ghostiakobilord2@gmail.com>
2024-01-12 01:15:24 +0400

    Updates

---
+++ assets/BookingConfirmed-195a510b.js
@@ -0,0 +1,1 @@
+import{s as o}from"./index-8c1f9154.js";const t={__name:"BookingConfirmed",setup(e){return console.log(o.state.isBookingConfirmed),(n,r)=>null}};export{t as default};
--- assets/BookingConfirmed-8eb7e940.js
+++
@@ -1,1 +0,0 @@
-import{s as o}from"./index-0c9a5808.js";const t={__name:"BookingConfirmed",setup(e){return console.log(o.state.isBookingConfirmed),(n,r)=>null}};export{t as default};
